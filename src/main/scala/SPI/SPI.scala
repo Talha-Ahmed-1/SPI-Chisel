e1cf44456eb01d2a829037d1edb865aa5f522ef9
Talha Ahmed <talhaahmedashraf@gmail.com>
2021-05-15 17:11:54 +0500

    buffer added

---
+++ src/main/scala/SPI/SPI.scala
@@ -0,0 +1,47 @@
+package SPI
+
+import chisel3._
+import chisel3.util._
+
+class SPI extends Module{
+    val io = IO(new SPI_IO)
+    val buffer = Module(new Buffer)
+
+    buffer.io.in <> io.in
+    io.out <> buffer.io.out
+}
+
+class SPI_IO extends Bundle{
+    val out = Decoupled(UInt(1.W))
+    val in = Flipped(Decoupled(UInt(1.W)))
+}
+
+class Buffer extends Module{
+    val io = IO(new Buffer_IO)
+
+    val free :: busy :: Nil = Enum(2)
+    val state = RegInit(free)
+    val data = RegInit(0.U)
+
+    io.in.ready := state === free
+    io.out.valid := state === busy
+
+    when(state === free){
+        when(io.in.valid){
+            data := io.in.bits
+            state := busy
+        }
+    }.otherwise{
+        when(io.out.ready){
+            io.out.bits := data
+            state := free
+        }
+    }
+    io.out.bits := DontCare
+}
+
+class Buffer_IO extends Bundle{
+    val out = Decoupled(UInt(1.W))
+    val in = Flipped(Decoupled(UInt(1.W)))
+
+}
---
+++ src/test/scala/SPI/SPITests.scala
@@ -0,0 +1,21 @@
+package SPI
+
+import chisel3._
+import chisel3 . util._
+import org.scalatest._
+import chiseltest._
+import chisel3.experimental.BundleLiterals._
+import chiseltest.experimental.TestOptionBuilder._
+import chiseltest.internal.VerilatorBackendAnnotation
+
+class SPITests extends FreeSpec with ChiselScalatestTester {
+
+  "SPI" in {
+    test(new SPI).withAnnotations(Seq(VerilatorBackendAnnotation)) { c =>
+        c.io.in.bits.poke(1.U)
+        c.io.in.valid.poke(1.B)
+        c.io.out.ready.poke(1.B)
+        c.clock.step(20)
+    }
+  }
+}
