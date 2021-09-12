d945f1db057c78f09447add21aa411c5d4fc4327
Talha Ahmed <talhaahmedashraf@gmail.com>
2021-09-12 23:16:33 +0500

    loopback test added by mosi wired to miso

--- src/test/scala/SPI/SpiWrapperTests.scala
+++ src/test/scala/SPI/SpiWrapperTests.scala
@@ -12,12 +12,24 @@ import chiseltest.internal.VerilatorBackendAnnotation
 
 
 
-class SpiWrapperTests extends FreeSpec with ChiselScalatestTester {
+class SpiWrapperTests extends FreeSpec with ChiselScalatestTester{
 
   "Spi Wrapper" in {
     test(new SpiWrapper()).withAnnotations(Seq(VerilatorBackendAnnotation)) { c =>
-        
-        c.clock.step(100)
+        c.io.dataRequest.bits.poke("b10111011101110111011101110111011".U)
+        c.io.dataRequest.valid.poke(1.B)
+        c.clock.step(1)
+        c.io.dataRequest.valid.poke(0.B)
+        c.io.dataResponse.ready.poke(1.B)
+        // c.io.dataResponse.ready.poke(1.B)
+        var count = 1
+        while(count != 1000) {
+            val mosi = c.io.mosi.peek()
+            c.io.miso.poke(mosi)
+            // println("This is the real answer ************"+c.io.mosi.peek())
+            c.clock.step(1)
+            count += 1
+        }
     }
   }
 }
